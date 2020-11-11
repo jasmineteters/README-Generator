f694522a723c81c5acb1782f96f2eed3636a07ea
Papaya Delight <jasmineteters2020@gmail.com>
2020-11-10 21:35:02 -0700

    got the questions written up. Played around with the formatting for the readme. wrote the writefile function.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# README-Generator
+# hard project this is
+## Description 
+this is for shortcuts
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributors](#contributors)
+* [License](#license)
+* [Contact](#contact)
+## Installation
+none
+## Usage 
+none
+## Contributors
+none
+## Tests
+none
+## Contact
+To view the code for this project visit the github profile below.
+Github Profile:[jasmineteters](jasmineteters)
+For any questions contact us via email.
+Email: [jasmineteters2020@gmail.com](mailto:jasmineteters2020@gmail.com)
--- index.js
+++ index.js
@@ -1,16 +1,98 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-];
+inquirer
+    .prompt([{
+            type: 'input',
+            message: 'What is the name of your Project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Describe your project?',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'What are the instructions for installation?',
+            name: 'instructions',
+        },
+        {
+            type: 'input',
+            message: 'Usage information details?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'What are your contribution guidelines?',
+            name: 'contributions',
+        },
+        {
+            type: 'input',
+            message: 'What are the test instructions?',
+            name: 'test',
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'username',
+        },
+        {
+            type: 'email',
+            message: 'What is your email address?',
+            name: 'email',
+        },
+    ]).then((response) => {
+        console.log(response);
+        const readData =
+            `# ${response.title}
+## Description 
+${response.description}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributors](#contributors)
+* [License](#license)
+* [Contact](#contact)
+## Installation
+${response.instructions}
+## Usage 
+${response.usage}
+## Contributors
+${response.contributions}
+## Tests
+${response.test}
+## Contact
+To view the code for this project visit the github profile below.
+Github Profile: [${response.username}](${response.username})
+For any questions contact us via email.
+Email: [${response.email}](mailto:${response.email})
+`;
+
+
+        const filename = "readme.md";
+
+        fs.writeFile(filename, readData, (err) =>
+            err ? console.log(err) : console.log("success")
+        );
+    });
+
+
+// // array of questions for user
+// const questions = [
+//     "Name :":
+// ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// function writeToFile(filename, data) {
 
-// function to initialize program
-function init() {
 
-}
+// }
 
-// function call to initialize program
-init();
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
